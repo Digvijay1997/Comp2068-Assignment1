08f91e09f6981740f7aead93045469deb566d9ef
Digvijay1997 <digvijay.singh1322420@gmail.com>
2017-02-14 23:03:01 -0500

    Addede files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Digvijay ' });
 });
+
+
+
+/* GET home page. */
+router.get('/About_Me', function(req, res, next) {
+    res.render('About_Me' );
+});
+
+
+/* GET home page. */
+router.get('/Contact', function(req, res, next) {
+    res.render('Contact' );
+});
+/* GET home page. */
+router.get('/Projects', function(req, res, next) {
+    res.render('Projects' );
+});/* GET home page. */
+router.get('/Services', function(req, res, next) {
+    res.render('Services' );
+});
+
 
 module.exports = router;
